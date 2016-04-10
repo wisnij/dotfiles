6d5f75485211e980682eeac9c886459c88fd4fa9
Jim Wisniewski <wisnij@gmail.com>
2016-04-09 21:58:20 -0400

    add la and lla

--- .config/bash/rc/11-alias.bash
+++ .config/bash/rc/11-alias.bash
@@ -1,4 +1,4 @@
-lsopts='-AF --color=auto'
+lsopts='-F --color=auto'
 lsopts_long='-l --time-style="+%Y-%m-%d %H:%M:%S"'
 if ls --help 2>/dev/null | grep -q group-directories-first; then
     lsopts="$lsopts --group-directories-first"
@@ -7,6 +7,9 @@ fi
 alias  l="ls $lsopts $lsopts_long --si"
 alias ll="ls $lsopts $lsopts_long"
 alias ls="ls $lsopts"
+
+alias  la='l -A'
+alias lla='ll -A'
 
 le () {
     l --color "$@" | egrep -v '^-r-.r-.r-.'
