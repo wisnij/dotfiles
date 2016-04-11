96e5325461e527c927221bd4efd98549f5d243a1
Jim Wisniewski <wisnij@gmail.com>
2016-04-11 15:07:28 -0400

    use cyan rather than blue for diffs

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -58,8 +58,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(diff-added ((t (:inherit diff-changed :foreground "blue"))))
- '(diff-added-face ((t (:inherit diff-changed :foreground "blue"))))
+ '(diff-added ((t (:inherit diff-changed :foreground "cyan"))))
+ '(diff-added-face ((t (:inherit diff-changed :foreground "cyan"))))
  '(diff-changed ((nil (:foreground "darkgreen"))))
  '(diff-changed-face ((nil (:foreground "darkgreen"))))
  '(diff-removed ((t (:inherit diff-changed :foreground "red"))))
