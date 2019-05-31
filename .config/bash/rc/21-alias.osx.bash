6a6072839930bc98a990b6dcb3d00f7bf4112a7d
Jim Wisniewski <wisnij@gmail.com>
2019-05-30 22:10:09 -0400

    only use simple ls aliases if we don't have GNU ls

--- .config/bash/rc/21-alias.osx.bash
+++ .config/bash/rc/21-alias.osx.bash
@@ -1,6 +1,8 @@
-lsopts='-AFG'
-lsopts_long='-lT'
+if ! ls --help >/dev/null 2>&1; then
+    lsopts='-AFG'
+    lsopts_long='-lT'
 
-alias  l="ls $lsopts $lsopts_long -h"
-alias ll="ls $lsopts $lsopts_long"
-alias ls="ls $lsopts"
+    alias  l="ls $lsopts $lsopts_long -h"
+    alias ll="ls $lsopts $lsopts_long"
+    alias ls="ls $lsopts"
+fi
