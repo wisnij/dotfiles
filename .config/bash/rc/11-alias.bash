4b7352632937541ac04f119589c187bf884d413d
Jim Wisniewski <wisnij@gmail.com>
2019-06-12 15:55:22 -0400

    localize $lsopts in function

--- .config/bash/rc/11-alias.bash
+++ .config/bash/rc/11-alias.bash
@@ -1,15 +1,19 @@
-lsopts='-F --color=auto'
-lsopts_long='-l --time-style="+%Y-%m-%d %H:%M:%S"'
-if ls --help 2>/dev/null | grep -q group-directories-first; then
-    lsopts="$lsopts --group-directories-first"
-fi
+_alias_ls () {
+    local lsopts='-F --color=auto'
+    local lsopts_long='-l --time-style="+%Y-%m-%d %H:%M:%S"'
+    if ls --help 2>/dev/null | grep -q group-directories-first; then
+        lsopts="$lsopts --group-directories-first"
+    fi
 
-alias  l="ls $lsopts $lsopts_long --si"
-alias ll="ls $lsopts $lsopts_long"
-alias ls="ls $lsopts"
+    alias  l="ls $lsopts $lsopts_long --si"
+    alias ll="ls $lsopts $lsopts_long"
+    alias ls="ls $lsopts"
 
-alias  la='l -A'
-alias lla='ll -A'
+    alias  la='l -A'
+    alias lla='ll -A'
+}
+
+_alias_ls
 
 le () {
     l --color "$@" | egrep -v '^-r-.r-.r-.'
