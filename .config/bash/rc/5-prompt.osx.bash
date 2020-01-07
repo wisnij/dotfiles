a63946b3c079fd06a2d2726de92fd8623a96eb1a
Jim Wisniewski <wisnij@gmail.com>
2020-01-07 16:58:23 -0500

    use sed -E for posix compatibility

--- .config/bash/rc/5-prompt.osx.bash
+++ .config/bash/rc/5-prompt.osx.bash
@@ -2,7 +2,7 @@ _titlebar_prompt () {
     local dir="${PWD/$HOME/~}"
     if [[ $TERM_PROGRAM == 'iTerm.app' ]]; then
         # shorten to fit in tabs
-        dir=$(sed -re 's/^.+(.{16})$/...\1/' <<<"$dir")
+        dir=$(sed -E -e 's/^.+(.{16})$/...\1/' <<<"$dir")
     fi
     echo -ne "\033];$dir\007"
 }
