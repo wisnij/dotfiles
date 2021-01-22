005ca52c5d8f53c78bf1c985fdf98aca06838cb7
Jim Wisniewski <wisnij@gmail.com>
2021-01-22 13:01:15 -0500

    move waitpid to a utils script

--- .config/bash/rc/12-functions.bash
+++ .config/bash/rc/12-functions.bash
@@ -108,20 +108,6 @@ todo () {
 }
 
 
-waitpid () {
-    local pid=$1
-    local seen=0
-    while [[ -e /proc/$pid ]]; do
-        sleep 1
-        seen=1
-    done
-    if [[ $seen -eq 0 ]]; then
-        echo "process $pid not found"
-        return 2
-    fi
-}
-
-
 writeable() {
     find "$@" -type f -writable '!' -name '*~'
 }
