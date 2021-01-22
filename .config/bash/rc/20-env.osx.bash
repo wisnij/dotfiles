52de824d601d8b705c65a865a6153a4c0f62f09c
Jim Wisniewski <wisnij@gmail.com>
2021-01-22 15:25:10 -0500

    add GNU utils to path if installed

--- .config/bash/rc/20-env.osx.bash
+++ .config/bash/rc/20-env.osx.bash
@@ -5,3 +5,17 @@ export MANPATH="$MANPATH:/usr/local/man"
 if [[ $TERM == 'screen-256color' ]]; then
     export TERM=screen
 fi
+
+_osx_gnu () {
+    # after "brew install coreutils findutils gawk gnu-getopt gnu-indent gnu-sed gnu-tar gnu-units gnutls grep"
+    local package
+    # XXX: removed gnu-sed to avoid incompatible differences vs built-in
+    for package in coreutils findutils gawk gnu-indent gnu-tar gnu-units grep; do
+        local dir="/usr/local/opt/$package/libexec/gnubin"
+        if [[ -d $dir ]]; then
+            export PATH="$dir:$PATH"
+        fi
+    done
+}
+
+_osx_gnu
