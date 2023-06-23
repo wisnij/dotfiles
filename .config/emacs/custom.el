89c70f9cefbc7116f98192d71995687fedbe434c
Jim Wisniewski <wisnij@gmail.com>
2023-06-23 03:43:48 -0400

    consolidate face settings into theme

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -63,18 +63,3 @@
  '(version-control t)
  '(visual-line-fringe-indicators '(empty-line nil))
  '(x-stretch-cursor t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(diff-added ((t (:inherit diff-changed :foreground "cyan"))))
- '(diff-added-face ((t (:inherit diff-changed :foreground "cyan"))))
- '(diff-changed ((nil (:foreground "darkgreen"))))
- '(diff-changed-face ((nil (:foreground "darkgreen"))))
- '(diff-removed ((t (:inherit diff-changed :foreground "red"))))
- '(diff-removed-face ((t (:inherit diff-changed :foreground "red"))))
- '(git-commit-comment-face ((t (:inherit font-lock-comment-face))))
- '(git-commit-overlong-summary-face ((t (:inherit font-lock-warning-face))))
- '(markdown-code-face ((t (:inherit nil)))))
