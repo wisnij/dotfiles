69fbbb62c793f41d37766f5a3a4d588e50b4ea2d
Jim Wisniewski <wisnij@gmail.com>
2023-07-03 14:53:21 -0400

    special handling for git dirs

--- .config/bash/rc.d/5-prompt.osx.bash
+++ .config/bash/rc.d/5-prompt.osx.bash
@@ -1,13 +1,25 @@
-_titlebar_prompt () {
-    local dir
-    if [[ ${BASH_VERSINFO} -ge 4 ]]; then
-        dir="${PWD/$HOME/\~}"
+_mac_titlebar_prompt () {
+    case $TERM in
+        screen*)
+            return
+            ;;
+    esac
+
+    local dir=$PWD
+    if git rev-parse --is-inside-work-tree >/dev/null; then
+        dir=$(basename $(git rev-parse --show-toplevel))
     else
-        dir="${PWD/$HOME/~}"
-    fi
-    if [[ $TERM_PROGRAM == 'iTerm.app' ]]; then
-        # shorten to fit in tabs
-        dir=$(sed -E -e 's/([^\/])[^\/]*\//\1\//g' <<<"$dir")
+        if [[ ${BASH_VERSINFO} -ge 4 ]]; then
+            dir="${dir/$HOME/\~}"
+        else
+            dir="${dir/$HOME/~}"
+        fi
+        if [[ $TERM_PROGRAM == 'iTerm.app' ]]; then
+            # shorten to fit in tabs
+            dir=$(sed -E -e 's/([^\/])[^\/]*\//\1\//g' <<<"$dir")
+        fi
     fi
     echo -ne "\033];$dir\007"
 }
+
+PROMPT_COMMAND="${PROMPT_COMMAND}_mac_titlebar_prompt"
