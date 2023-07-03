25c0917274892fb7442f34dd9123c19d934dbaed
Jim Wisniewski <wisnij@gmail.com>
2023-07-03 17:42:10 -0400

    blank custom-set-faces

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -63,3 +63,9 @@
  '(version-control t)
  '(visual-line-fringe-indicators '(empty-line nil))
  '(x-stretch-cursor t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
