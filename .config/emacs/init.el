f1be5c45a2099567c2727cbd31338ca9e4869c31
Jim Wisniewski <wisnij@gmail.com>
2023-07-28 17:26:03 -0400

    preserve-nl-back

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -670,7 +670,7 @@ mark it as unmodified."
 (global-set-key (kbd "M-Q") #'unfill-paragraph)
 (global-set-key (kbd "M-s") #'next-frame-window)
 (global-set-key (kbd "M-S") #'previous-frame-window)
-(global-set-key (kbd "M-SPC") (lambda () (interactive) (cycle-spacing -1 nil 'fast)))
+(global-set-key (kbd "M-SPC") (lambda () (interactive) (cycle-spacing -1 :preserve-nl-back 'fast)))
 (global-set-key (kbd "s-<down>") #'next-frame-window)
 (global-set-key (kbd "s-<left>") (lambda () (interactive) (other-frame -1)))
 (global-set-key (kbd "s-<right>") #'other-frame)
