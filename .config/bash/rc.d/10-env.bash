e5de0140d26699279ee13033776f31d73f5c6214
Jim Wisniewski <wisnij@gmail.com>
2024-07-02 00:58:32 -0400

    export XDG user dir env vars

--- .config/bash/rc.d/10-env.bash
+++ .config/bash/rc.d/10-env.bash
@@ -14,6 +14,18 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
 
+if [[ -e $XDG_CONFIG_HOME/user-dirs.dirs ]]; then
+    source "$XDG_CONFIG_HOME/user-dirs.dirs"
+    export XDG_DESKTOP_DIR
+    export XDG_DOCUMENTS_DIR
+    export XDG_DOWNLOAD_DIR
+    export XDG_MUSIC_DIR
+    export XDG_PICTURES_DIR
+    export XDG_PUBLICSHARE_DIR
+    export XDG_TEMPLATES_DIR
+    export XDG_VIDEOS_DIR
+fi
+
 # clear this out initially to set in 4-prompt.bash and others
 unset PROMPT_COMMAND
 
