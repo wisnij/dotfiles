9d6d5876ee4a49ad7ffd0844eafadc3becd97ff1
Jim Wisniewski <wisnij@gmail.com>
2025-08-05 20:37:42 -0400

    add smart-clone wrapper script for git smart-clone

--- common/.config/bash/rc.d/4-git.bash
+++ common/.config/bash/rc.d/4-git.bash
@@ -2,4 +2,11 @@ if type -t git >/dev/null; then
     alias gd='git d'
     alias gds='git ds'
     alias gg='git g'
+
+    smart-clone () {
+        local dir=$(git smart-clone "$@")
+        if [[ -n $dir ]]; then
+            cd "$dir"
+        fi
+    }
 fi
