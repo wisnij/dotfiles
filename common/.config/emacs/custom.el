a8386769b2587a4e31ec0498cc545af903921fbd
Jim Wisniewski <wisnij@gmail.com>
2026-05-26 19:41:12 -0400

    use the same arrow for wrapping long lines and visual-line-mode

--- common/.config/emacs/custom.el
+++ common/.config/emacs/custom.el
@@ -78,7 +78,7 @@
  '(use-short-answers t)
  '(version-control t)
  '(view-read-only t)
- '(visual-line-fringe-indicators '(empty-line nil))
+ '(visual-line-fringe-indicators '(left-curly-arrow nil))
  '(vterm-set-bold-hightbright t)
  '(x-stretch-cursor t))
 (custom-set-faces
